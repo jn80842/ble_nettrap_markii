950c7bc3f92cb9891b68d157f13cd984556a9c74
Julie Newcomb <jn80842@gmail.com>
2014-11-09 16:02:56 -0800

    code reorg

--- index.js
+++ index.js
@@ -23,15 +23,112 @@ app.use(express.static(__dirname + '/public'));
 
 
 io.on('connection', function (socket) {
-  var addedUser = false;
+  // start write ble function
+  function write_ble(data) {
 
-  socket.on('reset net', function(data) {
+    noble.startScanning();
 
+    noble.on('discover', function(peripheral) {
+     console.log('peripheral with UUID ' + peripheral.uuid + ' found');
+     if (peripheral.uuid == NETTRAP_PERIPHERAL_UUID) {
+      explore(peripheral);
+    }
+    });
+
+    function explore(peripheral) {
+      console.log('services and characteristics:');
+    
+      var chrcData = {};
+    
+      peripheral.on('disconnect', function() {
+        console.log("disarmed is " + chrcData.disarmed + " dropped is " + chrcData.dropped);
+        socket.emit('ble info',chrcData);
+        noble.stopScanning();
+      });
+    
+      peripheral.connect(function(error) {
+        peripheral.discoverServices([], function(error, services) {
+          var serviceIndex = 0;
+    
+          async.whilst(
+            function () {
+              return (serviceIndex < services.length);
+            },
+            function(callback) {
+              var service = services[serviceIndex];
+              var serviceInfo = service.uuid;
+    
+              if (service.name) {
+                serviceInfo += ' (' + service.name + ')';
+              }
+              console.log(serviceInfo);
+    
+              service.discoverCharacteristics([], function(error, characteristics) {
+                var characteristicIndex = 0;
+    
+                async.whilst(
+                  function () {
+                    return (characteristicIndex < characteristics.length);
+                  },
+                  function(callback) {
+                    var characteristic = characteristics[characteristicIndex];
+                    var characteristicInfo = '  ' + characteristic.uuid;
+    
+                    if (characteristic.name) {
+                      characteristicInfo += ' (' + characteristic.name + ')';
+                    }
+    
+                    async.series([
+                      function(callback) {
+                            characteristicInfo += '\n    properties  ' + characteristic.properties.join(', ');
+    
+                        if (characteristic.properties.indexOf('write') !== -1) {
+                          if (characteristic.uuid == NETTRAP_IS_NET_DISARMED_CHARACTERISTIC_UUID) {
+                            var writeValue = new ArrayBuffer(1);
+                            var writeBytes = new Uint8Array(writeValue);
+                            writeBytes[0] = data.disarmed;
+
+                            characteristic.write(writeValue,false,function(err) {
+                              console.log("wrote to disarmed characteristic");
+                            });                          
+                          }
+                          if (characteristic.uuid == NETTRAP_WAS_NET_DROPPED_CHARACTERISTIC_UUID) {
+                            var writeValue = new ArrayBuffer(1);
+                            var writeBytes = new Uint8Array(writeValue);
+                            writeBytes[0] = data.dropped;
+                            characteristic.write(writeValue,false,function(error) {
+                              console.log("wrote to dropped characteristic");
+                            })
+                          }
+                        } else {
+                          callback();
+                        }
+                      },
+                      function() {
+                        console.log(characteristicInfo);
+                        characteristicIndex++;
+                        callback();
+                      }
+                    ]);
+                  },
+                  function(error) {
+                    serviceIndex++;
+                    callback();
+                  }
+                );
+              });
+            },
+            function (err) {
+              peripheral.disconnect();
+            }
+          );
+        });
+      });
+    }
   };
-
-  // when the client emits 'new message', this listens and executes
-  socket.on('read ble', function (data) {
-
+  //end write ble function
+  // start read ble function
+  function read_ble() {
     noble.startScanning();
 
     noble.on('discover', function(peripheral) {
@@ -129,21 +226,21 @@ io.on('connection', function (socket) {
           );
         });
       });
-    }
+    } 
+  }; //close read ble function
+
+  socket.on('reset net', function(data) {
+      console.log("client asked to reset net");
+  });
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('read ble', function (data) {
+    read_ble();
   });
 
   // when the user disconnects.. perform this
   socket.on('disconnect', function () {
-    // remove the username from global usernames list
-    if (addedUser) {
-      delete usernames[socket.username];
-      --numUsers;
-
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-    }
+    console.log("socket is disconnected");
+    //noble.stopScanning();
   });
 });
